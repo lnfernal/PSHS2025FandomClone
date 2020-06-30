226f7bf90220799639637b94d1bd3d4717b63f9d
Asriel Dreemurr <52394592+RedBlazerFlame@users.noreply.github.com>
2020-06-30 10:08:57 +0800

    Change Links... Again
    Links still weren't working; Changed to link back to GitHub Pages

--- header.js
+++ header.js
@@ -1,14 +1,14 @@
 document.write(`<div class="dropdown left-border">
 <button class="dropbtn">Games</button>
 <div class="dropcontent">
-    <a href="https://github.com/RedBlazerFlame/PSHS2025FandomClone/fandoms/games/undertale">Undertale</a>
-    <a href="https://github.com/RedBlazerFlame/PSHS2025FandomClone/fandoms/games/undertale_aus">Undertale AUs</a>
+    <a href="https://redblazerflame.github.io/PSHS2025FandomClone/fandoms/games/undertale">Undertale</a>
+    <a href="https://redblazerflame.github.io/PSHS2025FandomClone/fandoms/games/undertale_aus">Undertale AUs</a>
 </div>
 </div>
 <div class="dropdown right-border">
 <button class="dropbtn">Others</button>
 <div class="dropcontent">
-    <a href="https://github.com/RedBlazerFlame/PSHS2025FandomClone/fandoms/others/ruby">Ruby</a>
+    <a href="https://redblazerflame.github.io/PSHS2025FandomClone/fandoms/others/ruby">Ruby</a>
     
 </div>
 </div>`)
