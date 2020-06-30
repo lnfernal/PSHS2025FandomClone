1cce6fdb14ee00c3e5b2c8a2401539805a0e57e8
Asriel Dreemurr <52394592+RedBlazerFlame@users.noreply.github.com>
2020-06-30 10:20:07 +0800

    added target="_blank"
    creates a new tab instead of redirecting

--- header.js
+++ header.js
@@ -1,14 +1,14 @@
 document.write(`<div class="dropdown left-border">
 <button class="dropbtn">Games</button>
 <div class="dropcontent">
-    <a href="/PSHS2025FandomClone/fandoms/games/undertale">Undertale</a>
-    <a href="/PSHS2025FandomClone/fandoms/games/undertale_aus">Undertale AUs</a>
+    <a target="_blank" href="/PSHS2025FandomClone/fandoms/games/undertale">Undertale</a>
+    <a target="_blank" href="/PSHS2025FandomClone/fandoms/games/undertale_aus">Undertale AUs</a>
 </div>
 </div>
 <div class="dropdown right-border">
 <button class="dropbtn">Others</button>
 <div class="dropcontent">
-    <a href="/PSHS2025FandomClone/fandoms/others/ruby">Ruby</a>
+    <a target="_blank" href="/PSHS2025FandomClone/fandoms/others/ruby">Ruby</a>
     
 </div>
 </div>`)
